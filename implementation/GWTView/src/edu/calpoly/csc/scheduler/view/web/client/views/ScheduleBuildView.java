e3c9587501d8f2bd2c76273ffc689ce1c4cae76a
tyero <tyero@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2011-10-22 21:46:48 +0000

    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@641 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/views/ScheduleBuildView.java
+++ implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/views/ScheduleBuildView.java
@@ -1,18 +1,33 @@
 package edu.calpoly.csc.scheduler.view.web.client.views;
 
+import java.util.ArrayList;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import edu.calpoly.csc.scheduler.view.web.client.GreetingServiceAsync;
+import edu.calpoly.csc.scheduler.view.web.shared.CourseGWT;
+import edu.calpoly.csc.scheduler.view.web.shared.gwtScheduleItem;
 
 public class ScheduleBuildView extends ScrollPanel {
 	private GreetingServiceAsync service;
 
+	private ListBox listBoxAvailableCourses = new ListBox();
+	private ListBox listBoxIncludedCourses = new ListBox();
+	private Button buttonGenerate = new Button();
+	private FlexTable flexTableCalendar = new FlexTable();
+	
 	public ScheduleBuildView(Panel container, GreetingServiceAsync greetingService, String quarterID) {
 		this.service = greetingService;
 	}
@@ -21,29 +36,65 @@ public class ScheduleBuildView extends ScrollPanel {
 	public void onLoad() {
 		super.onLoad();
 
+		// fill all of available space
 		setWidth("100%");
 		setHeight("100%");
-
 		
-		DockPanel dp = new DockPanel();
+		// make sure panel is clear of items
+		this.clear();
 		
+		VerticalPanel vp = new VerticalPanel();									
+		HorizontalPanel hp = new HorizontalPanel();		
 		
-		ListBox listBoxAvailableCourses = new ListBox();		
-		dp.add(listBoxAvailableCourses);
-		
-		ListBox listBoxIncludedCourses = new ListBox();		
-		dp.add(listBoxIncludedCourses);
-		
-		Button buttonGenerate = new Button();
+		hp.add(listBoxAvailableCourses);
+		hp.add(flexTableCalendar);
+		hp.add(listBoxIncludedCourses);
+			
+		buttonGenerate.setText("Generate Schedule");
 		buttonGenerate.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				
+				
+				service.generateSchedule(new AsyncCallback<ArrayList<gwtScheduleItem>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO something bad happened
+						Window.alert("Generate schedule failed.");
+					}
+
+					@Override
+					public void onSuccess(ArrayList<gwtScheduleItem> result) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				});
 			}
 		});
+				
+		vp.add(hp);
+		vp.add(buttonGenerate);
 		
-		this.add(dp);
-//		service.generateSchedule(new Callback() {
-//			
-//		});
+		this.add(vp);
+
+		service.getCourses(new AsyncCallback<ArrayList<CourseGWT>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO something bad happened
+				Window.alert("Could not get available courses");
+			}
+
+			@Override
+			public void onSuccess(ArrayList<CourseGWT> result) {
+				// fill listbox with courses
+				for (int i = 0; i < result.size(); i++) {					
+					listBoxAvailableCourses.insertItem(result.get(i).getCourseName(), i);				
+				}
+				
+			}
+			
+		});
 	}
 }
