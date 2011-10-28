5013620b09c3ba3a836eccf69cca2183e04e5413
eliebowi <eliebowi@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2011-10-28 20:32:23 +0000

    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@704 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/model/java/edu/calpoly/csc/scheduler/model/Scheduler.java
+++ implementation/model/java/edu/calpoly/csc/scheduler/model/Scheduler.java
@@ -1,11 +1,11 @@
 package edu.calpoly.csc.scheduler.model;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 import edu.calpoly.csc.scheduler.model.db.*;
-import edu.calpoly.csc.scheduler.model.db.cdb.*;
-import edu.calpoly.csc.scheduler.model.db.idb.*;
-import edu.calpoly.csc.scheduler.model.db.ldb.*;
+import edu.calpoly.csc.scheduler.model.schedule.*;
 
 /**
  * The top-level model. Views will be passed this object when they are 
@@ -20,112 +20,42 @@ public class Scheduler implements Serializable
 {
    public static final int serialVersionUID = 42;
    
-   /**
-    * Our course database
-    */
-   public CourseDB cdb;
-   /**
-    * Our instrucor database
-    */
-   public InstructorDB idb;
-   /**
-    * Our location database
-    */
-   public LocationDB ldb;
+   private Database db;
    
-   /**
-    * Our preference database
-    */
-   public Object pdb;
-   
-   public Scheduler (String url)
+   public Scheduler (String username, Integer sid)
    {
+      db = new Database();
+   }
+   
+   public Map<Integer, String> getSchedules (String userId)
+   {
+      String dept = db.getDept(userId);
       
-   }
-
-   /**
-    * Returns the course db of this model
-    * 
-    * @return the course db of this model
-    */
-   public CourseDB getCourseDB ()
-   {
-      return cdb;
-   }
-
-   /**
-    * Sets the course db
-    *
-    * @param cdb the course db to set our cdb to
-    */
-   public void setCourseDB (CourseDB cdb)
-   {
-      this.cdb = cdb;
-   }
-
-   /**
-    * Returns the instructor db of this model
-    * 
-    * @return the instructor db of this model
-    */
-   public InstructorDB getInstructorDB ()
-   {
-      return idb;
-   }
-
-   /**
-    * Sets the instructor db to the given parameter.
-    *
-    * @param idb instructor db to set our idb to
-    */
-   public void setInstructorDB (InstructorDB idb)
-   {
-      this.idb = idb;
-   }
-
-   /**
-    * Returns the location db of this model
-    * 
-    * @return the location db of this model
-    */
-   public LocationDB getLocationDB ()
-   {
-      return ldb;
-   }
-
-   /**
-    * Sets the location db to the given parameter
-    *
-    * @param ldb location db to set our ldb to
-    */
-   public void setLocationDB (LocationDB ldb)
-   {
-      this.ldb = ldb;
-   }
-
-   /**
-    * Returns the preference db of this model
-    * 
-    * @return the preference db of this model
-    */
-   public Object getPreferenceDB ()
-   {
-      return pdb;
-   }
-
-   /**
-    * Sets the preference db to the given parameter
-    *
-    * @param pdb preference db to set our pdb to
-    */
-   public void setPdb (Object pdb)
-   {
-      this.pdb = pdb;
+      return db.getSchedules(dept);
    }
    
+   /**
+    * Returns the db
+    * 
+    * @return the db
+    */
+   public Database getDb ()
+   {
+      return db;
+   }
+
+   /**
+    * Sets the db to the given parameter.
+    *
+    * @param db the db to set
+    */
+   public void setDb (Database db)
+   {
+      this.db = db;
+   }
+
    public static void main (String[] args)
    {
       Database db = new Database();
-      
    }
 }
