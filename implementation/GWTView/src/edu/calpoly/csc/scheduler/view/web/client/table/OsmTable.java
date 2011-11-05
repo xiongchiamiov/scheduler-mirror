3ced54298c73097de15b8e902243cd790e6e2e34
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2011-11-05 22:10:16 +0000

    added osmtable
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@997 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

---
+++ implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/table/OsmTable.java
@@ -0,0 +1,45 @@
+package edu.calpoly.csc.scheduler.view.web.client.table;
+
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class OsmTable<ObjectType> extends VerticalPanel {
+	public interface IColumn<ObjectType> {
+		String getName();
+		Widget createCellWidget(ObjectType object);
+		int compare(ObjectType a, ObjectType b);
+	}
+	
+	public interface Column<ObjectType, ValType> extends IColumn<ObjectType> {
+		ValType getValue(ObjectType object);
+		void setValue(ObjectType object, ValType newValue);
+	}
+	
+	FlexTable table;
+	IColumn<ObjectType> columns[];
+	ArrayList<ObjectType> objects;
+	
+	public OsmTable(IColumn<ObjectType> columns[]) {
+		table = new FlexTable();
+		this.columns = columns;
+		
+		for (int colIndex = 0; colIndex < columns.length; colIndex++) {
+			table.setWidget(0, colIndex, new HTML(columns[colIndex].getName()));
+		}
+		
+		objects = new ArrayList<ObjectType>();
+	}
+	
+	void addRow(ObjectType object) {
+		int newObjectIndex = objects.size();
+		int newRowIndex = newObjectIndex + 1;
+		objects.add(object);
+		for (int colIndex = 0; colIndex < columns.length; colIndex++) {
+			table.setWidget(newRowIndex, colIndex, columns[colIndex].createCellWidget(object));
+		}
+	}
+}
