bd1ac6c4619b6a22ba3392d0a2755e0fdd2767c2
dseltzer <dseltzer@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2011-11-11 02:43:58 +0000

    Service updates
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1094 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/table/ButtonColumn.java
+++ implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/table/ButtonColumn.java
@@ -7,24 +7,26 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class ButtonColumn<ObjectType extends Comparable<ObjectType>> extends OsmTable.Column<ObjectType> {
 	public interface ClickCallback<ObjectType> {
-		void buttonClickedForObject(ObjectType object);
+		void buttonClickedForObject(ObjectType object, Button button);
+		String initialLabel(ObjectType object);
 	}
 	
 	private ClickCallback<ObjectType> clickHandler;
-	private String buttonLabel;
 	
-	public ButtonColumn(String name, String width, String buttonLabel, ClickCallback<ObjectType> click) {
+	public ButtonColumn(String name, String width, ClickCallback<ObjectType> click) {
 		super(name, width, null);
 		this.clickHandler = click;
-		this.buttonLabel = buttonLabel;
 	}
 	
 	public Widget createCellWidget(final ObjectType object) {
-		return new Button(buttonLabel, new ClickHandler() {
+		final Button button = new Button(clickHandler.initialLabel(object));
+		button.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				clickHandler.buttonClickedForObject(object);
+				clickHandler.buttonClickedForObject(object, button);
 			}
 		});
+		
+		return button;
 	}
 }
