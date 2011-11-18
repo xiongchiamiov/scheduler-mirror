26df01187f310150c9aaddab4ff6ffcc98fb64a9
tyhollan <tyhollan@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2011-11-18 19:50:14 +0000

    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1312 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/model/java/edu/calpoly/csc/scheduler/model/db/DbData.java
+++ implementation/model/java/edu/calpoly/csc/scheduler/model/db/DbData.java
@@ -1,64 +1,68 @@
 package edu.calpoly.csc.scheduler.model.db;
 
 /**
- *
+ * 
  * @author Eric Liebowitz
  * @version Oct 22, 2011
  */
 public abstract class DbData
 {
-   private Integer scheduleId;
-   private String note;
-   
+   public static final String SCHEDULEID = "scheduleid";
+   public static final String NOTE       = "note";
+
+   private Integer            scheduleId;
+   private String             note;
+
    /**
     * Returns the scheduleId
     * 
     * @return the scheduleId
     */
-   public Integer getScheduleId ()
+   public Integer getScheduleId()
    {
       return scheduleId;
    }
 
    /**
     * Sets the scheduleId to the given parameter.
-    *
-    * @param scheduleId the scheduleId to set
+    * 
+    * @param scheduleId
+    *           the scheduleId to set
     */
-   public void setScheduleId (int scheduleId)
+   public void setScheduleId(int scheduleId)
    {
       this.scheduleId = scheduleId;
    }
-   
+
    /**
     * Returns the note
     * 
     * @return the note
     */
-   public String getNote ()
+   public String getNote()
    {
-	   if(note == null)
-	   {
-		   note = "";
-	   }
-	   return note;
+      if (note == null)
+      {
+         note = "";
+      }
+      return note;
    }
-   
+
    /**
     * Sets the note to the given parameter.
-    *
-    * @param note the note to set
+    * 
+    * @param note
+    *           the note to set
     */
-   public void setNote (String note)
+   public void setNote(String note)
    {
-	   if(note == null)
-	   {
-		   note = "";
-	   }
-	   this.note = note;
+      if (note == null)
+      {
+         note = "";
+      }
+      this.note = note;
    }
 
-   public abstract void verify () throws NullDataException;
-
+   public abstract void verify() throws NullDataException;
 
 }
