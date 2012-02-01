af229d0f086e4e7ed4aee965a7337bb30dd73748
abarmstr <abarmstr@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-01 23:40:23 +0000

    Forgot to turn off debug mode in last commit.  Oops.
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1768 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/model/java/edu/calpoly/csc/scheduler/model/schedule/Schedule.java
+++ implementation/model/java/edu/calpoly/csc/scheduler/model/schedule/Schedule.java
@@ -229,7 +229,7 @@ public class Schedule extends DbData implements Serializable
 	/**
     * Used for debugging. Toggle it to get debugging output
     */
-   private static final boolean DEBUG = true; // !true == false ; )
+   private static final boolean DEBUG = !true; // !true == false ; )
    
    /**
     * Prints a message to STDERR if DEBUG is true
