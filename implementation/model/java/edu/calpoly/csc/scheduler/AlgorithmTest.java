fdf57476cb189dd66255c34fd5d3630b2c9de6b0
abarmstr <abarmstr@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-01 23:09:51 +0000

    Test to make sure scheduling algorithm is working
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1766 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

---
+++ implementation/model/java/edu/calpoly/csc/scheduler/AlgorithmTest.java
@@ -0,0 +1,115 @@
+package edu.calpoly.csc.scheduler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.calpoly.csc.scheduler.model.Model;
+import edu.calpoly.csc.scheduler.model.db.Time;
+import edu.calpoly.csc.scheduler.model.db.cdb.Course;
+import edu.calpoly.csc.scheduler.model.db.idb.CoursePreference;
+import edu.calpoly.csc.scheduler.model.db.idb.Instructor;
+import edu.calpoly.csc.scheduler.model.db.idb.TimePreference;
+import edu.calpoly.csc.scheduler.model.db.udb.UserData;
+import edu.calpoly.csc.scheduler.model.schedule.Day;
+import edu.calpoly.csc.scheduler.model.schedule.Schedule;
+import edu.calpoly.csc.scheduler.model.schedule.Week;
+import edu.calpoly.csc.scheduler.model.schedule.WeekAvail;
+
+public class AlgorithmTest {
+
+	public static void main(String[] args) {
+		Model model = new Model("adam");
+		Map<String, UserData> availableSchedules = model.getSchedules();
+		for (String avail : availableSchedules.keySet())
+			System.out.println(avail);
+		assert(availableSchedules.containsKey("Algorithm Test Schedule"));
+		model.openExistingSchedule("Algorithm Test Schedule");
+		Schedule sched = model.getSchedule();
+		
+		List<Course> courses = generateCourseList();
+		List<Instructor> instructors = generateInstructorList(courses);
+		
+		sched.setiSourceList(instructors);
+		
+		sched.generate(courses);
+		
+		printAllScheduledCourseInfo(sched);
+	}
+	
+	public static List<Course> generateCourseList() {
+		List<Course> courses = new ArrayList<Course>();
+		
+		Course lecture = new Course("Intro to Aerodynamics", "AERO", "101");
+		lecture.setEnrollment(30);
+		lecture.setLectureID(-1);
+		lecture.setDbid(1000);
+		lecture.setLength(6);
+		lecture.setNumOfSections(1);
+		lecture.setScu(4);
+		lecture.setWtu(4);
+		lecture.setType(Course.CourseType.LEC);
+		Week week = new Week(new Day[]{Day.MON, Day.WED, Day.FRI});
+		lecture.setDays(week);
+		courses.add(lecture);
+		
+		Course lab = new Course("Intro to Aerodynamics Lab", "AERO", "101");
+		lab.setEnrollment(30);
+		lab.setLectureID(1000);
+		lab.setLength(6);
+		lab.setNumOfSections(1);
+		lab.setScu(4);
+		lab.setWtu(4);
+		lab.setType(Course.CourseType.LAB);
+		lab.setTetheredToLecture(true);
+		Week week2 = new Week(new Day[]{Day.MON, Day.WED, Day.FRI});
+		lab.setDays(week2);
+		courses.add(lab);
+		
+		return courses;
+	}
+	
+	public static List<Instructor> generateInstructorList(List<Course> courses) {
+		List<Instructor> instructors = new ArrayList<Instructor>();
+		
+		Instructor instructor = new Instructor();
+		instructor.setFirstName("Adam");
+		instructor.setLastName("Armstrong");
+		instructor.setCurWtu(0);
+		instructor.setDisability(false);
+		instructor.setMaxWtu(50);
+		instructor.setAvailability(new WeekAvail());
+		instructor.setUserID("adam");
+		
+		for(Course c : courses) {
+			instructor.addCoursePreference(new CoursePreference(c, 10));
+		}
+		
+		HashMap<Day, LinkedHashMap<Time, TimePreference>> tps = new HashMap<Day, LinkedHashMap<Time, TimePreference>>();
+		LinkedHashMap<Time, TimePreference> times = new LinkedHashMap<Time, TimePreference>();
+		times.put(new Time(10, 0), new TimePreference(new Time(10, 0), 10));
+		times.put(new Time(10, 30), new TimePreference(new Time(10, 30), 10));
+		times.put(new Time(11, 0), new TimePreference(new Time(11, 0), 10));
+		times.put(new Time(11, 30), new TimePreference(new Time(11, 30), 10));
+		times.put(new Time(12, 0), new TimePreference(new Time(12, 0), 10));
+		times.put(new Time(12, 30), new TimePreference(new Time(12, 30), 10));
+		
+		tps.put(Day.MON, times);
+		tps.put(Day.TUE, times);
+		tps.put(Day.WED, times);
+		tps.put(Day.THU, times);
+		tps.put(Day.FRI, times);
+
+		instructor.setTimePreferences(tps);
+		
+		instructors.add(instructor);
+		
+		return instructors;
+	}
+	
+	public static void printAllScheduledCourseInfo(Schedule sched) {
+
+	}
+}
