52947e4681e536db0d7de6259c983ad2cd6eb603
jmhand <jmhand@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-03 07:56:39 +0000

    -Added CSV and PDF icons -Added Popup for "Download As ..." (Popup does not have functionality yet) in SelectScheduleView -Updated .css file with style used in "Download As.." Popup
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1783 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/views/SelectScheduleView.java
+++ implementation/GWTView/src/edu/calpoly/csc/scheduler/view/web/client/views/SelectScheduleView.java
@@ -17,9 +17,14 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
@@ -119,6 +124,11 @@ public class SelectScheduleView extends VerticalPanel implements IViewContents {
 		
 		MenuItem exportItem = new MenuItem("Download As...", true, new Command() {
 			public void execute() {
+				
+				displayDownloadAsPopup();
+				
+				//TODO integrate with DisplayDownloadAsPopup()
+				/*
 				service.exportCSV(new AsyncCallback<Integer>() {
 					public void onFailure(Throwable caught) {
 						Window.alert("Error exporting to CSV: 1");
@@ -131,6 +141,10 @@ public class SelectScheduleView extends VerticalPanel implements IViewContents {
 								+ "param" + "=" + result);
 					}
 				});
+				*/
+				
+				
+				
 			}
 		});
 		
@@ -498,6 +512,136 @@ public class SelectScheduleView extends VerticalPanel implements IViewContents {
 		db.setWidget(fp);
 		db.center();
 		db.show();
+	}
+	
+	
+	/**
+	 * Displays a popup to download schedule.
+	 */
+	public void displayDownloadAsPopup()
+	{	
+		final DialogBox db = new DialogBox();
+		VerticalPanel mainVerticalPanel = new VerticalPanel();
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		mainVerticalPanel.add(verticalPanel);
+		
+		HorizontalPanel typeSelectorPanel = new HorizontalPanel();
+		verticalPanel.add(typeSelectorPanel);
+		
+		final FocusPanel csvFocusPanel = new FocusPanel();
+		csvFocusPanel.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				//TODO Save Selection
+			}
+		});
+		
+		FocusPanel pdfFocusPanel = new FocusPanel();
+		pdfFocusPanel.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				//TODO Save Selection
+			}
+		});
+		
+		pdfFocusPanel.setStyleName("exportChoice");
+		typeSelectorPanel.add(pdfFocusPanel);
+		pdfFocusPanel.setSize("", "");
+		
+		VerticalPanel pdfPanel = new VerticalPanel();
+		pdfFocusPanel.setWidget(pdfPanel);
+		pdfPanel.setSize("100px", "100px");
+		
+		Image PDFIcon = new Image("imgs/pdf-icon.png");
+		pdfPanel.add(PDFIcon);
+		PDFIcon.setSize("100px", "100px");
+		
+		Label lblCSV = new Label("PDF");
+		pdfPanel.add(lblCSV);
+		pdfPanel.setCellHorizontalAlignment(lblCSV, HasHorizontalAlignment.ALIGN_CENTER);
+		csvFocusPanel.setStyleName("exportChoice");
+		typeSelectorPanel.add(csvFocusPanel);
+		typeSelectorPanel.setCellHorizontalAlignment(csvFocusPanel, HasHorizontalAlignment.ALIGN_CENTER);
+		csvFocusPanel.setSize("", "");
+
+		VerticalPanel csvPanel = new VerticalPanel();
+		csvFocusPanel.setWidget(csvPanel);
+		csvPanel.setSize("100px", "100px");
+
+		Image CSVIcon = new Image("imgs/csv-icon.png");
+		csvPanel.add(CSVIcon);
+		CSVIcon.setSize("100px", "100px");
+
+		Label lblNewLabel = new Label("Excel (CSV)");
+		csvPanel.add(lblNewLabel);
+		csvPanel.setCellHorizontalAlignment(lblNewLabel, HasHorizontalAlignment.ALIGN_CENTER);
+		
+		final HorizontalPanel Buttons = new HorizontalPanel();
+		Buttons.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+		
+		
+		mainVerticalPanel.add(Buttons);
+		Buttons.setWidth("102px");
+		mainVerticalPanel.setCellHorizontalAlignment(Buttons, HasHorizontalAlignment.ALIGN_CENTER);
+		
+		final Button cancelButton = new Button("Cancel", new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				db.hide();
+			}
+		});
+		
+		
+		final Button nextButton = new Button("Next", new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+
+				db.hide();
+				//TODO Add Selection between PDF and CSV options
+
+				//Temporary dialogue box until CSV functionality is officially integrate
+				
+				final DialogBox TODOdb = new DialogBox();
+				VerticalPanel TODOverticalPanel = new VerticalPanel();
+				VerticalPanel TODOmainVerticalPanel = new VerticalPanel();
+
+				TODOmainVerticalPanel.add(TODOverticalPanel);
+				TODOmainVerticalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+				
+				Label lblTODO = new Label("This feature is not yet implemented.");
+				TODOmainVerticalPanel.add(lblTODO);
+				
+				TODOdb.setText("Not yet implemented");
+				
+				final Button TODOcancelButton = new Button("Cancel", new ClickHandler() {
+					@Override
+					public void onClick(ClickEvent event) {
+						TODOdb.hide();
+					}
+				});
+				
+				TODOmainVerticalPanel.add(TODOcancelButton);
+
+				
+				TODOdb.setWidget(TODOmainVerticalPanel);
+				TODOdb.center();
+				TODOdb.show();
+			}
+		});
+		
+		
+		Buttons.add(cancelButton);
+		Buttons.add(nextButton);
+		nextButton.setWidth("65px");
+		
+		db.setText("Download As");
+		db.setWidget(mainVerticalPanel);
+		mainVerticalPanel.setSize("103px", "23px");
+		
+	
+		db.center();
+		db.show();
+
 	}
 	
 	/**
