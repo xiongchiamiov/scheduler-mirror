0040228bb2d2a96b99e18ecbc07dda2a10c7b9ce
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-13 17:58:22 +0000

    removing old files from branch
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1866 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/Model.java
+++ implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/Model.java
@@ -1,191 +1,26 @@
 package edu.calpoly.csc.scheduler.model;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import edu.calpoly.csc.scheduler.model.db.IDatabase;
+import edu.calpoly.csc.scheduler.model.db.IDatabase.NotFoundException;
 
-import edu.calpoly.csc.scheduler.model.db.Database;
-import edu.calpoly.csc.scheduler.model.db.cdb.Course;
-import edu.calpoly.csc.scheduler.model.db.idb.Instructor;
-import edu.calpoly.csc.scheduler.model.db.ldb.Location;
-import edu.calpoly.csc.scheduler.model.db.udb.UserData;
-import edu.calpoly.csc.scheduler.model.schedule.Schedule;
+public class Model {
+	final IDatabase database;
+	
+	public Model(IDatabase database) {
+		this.database = database;
+	}
 
-/**
- * The top-level model. Views will be passed this object when they are
- * instantiated. Any view interaction w/ underlying data will be done via this
- * class. Thus, the getters/setters here will determine how the view and the
- * model can interact.
- * 
- * @author Eric Liebowitz
- * @version Oct 3, 2011
- */
-public class Model implements Serializable
-{
-   public static final long      serialVersionUID = 42;
+	public String generateUnusedUsername() { return database.generateUnusedUsername(); }
 
-   private Database              db;
-   private String                userId;
-   private Map<String, UserData> sched_map        = new HashMap<String, UserData>();
+	public User insertUser(String username, boolean b) {
+		return new User(database.insertUser(username, b));
+	}
 
-   public Model(String userId)
-   {
-      this.userId = userId;
-      this.db = new Database(userId);
-   }
+	public Document insertDocument(String name) {
+		return new Document(database.insertDocument(name));
+	}
 
-   public Schedule getSchedule()
-   {
-      return db.getScheduleDB().getSchedule(db.getScheduleID());
-   }
-
-   /**
-    * Returns a list of schedules which a given user has access to.
-    * 
-    * @return Map, keyed by schedule ids which yields schedule names.
-    * 
-    * @see Database#getSchedules(String)
-    */
-   public Map<String, UserData> getSchedules()
-   {
-      this.sched_map = db.getSchedules();
-      return sched_map;
-   }
-
-   public Schedule loadSchedule(int sid)
-   {
-      return db.getScheduleDB().getSchedule(sid);
-   }
-
-   public void openExistingSchedule(String scheduleName)
-   {
-      int sid = this.sched_map.get(scheduleName).getScheduleDBId();
-      db.openDB(sid, scheduleName);
-   }
-
-   public void openExistingSchedule(Integer sid)
-   {
-      db.openDB(sid, "");
-   }
-
-   public void openNewSchedule(String scheduleName)
-   {
-      db.openDB(-1, scheduleName);
-   }
-
-   public void saveInstructor(Instructor instructor)
-   {
-      db.getInstructorDB().saveData(instructor);
-   }
-
-   public Collection<Instructor> getInstructors()
-   {
-      return db.getInstructorDB().getData();
-   }
-
-   public Integer getScheduleID()
-   {
-      return db.getScheduleID();
-   }
-
-   public void removeInstructor(Instructor instructor)
-   {
-      db.getInstructorDB().removeData(instructor);
-   }
-
-   public ArrayList<Course> getCourses()
-   {
-      return db.getCourseDB().getData();
-   }
-
-   public Collection<Location> getLocations()
-   {
-      return db.getLocationDB().getData();
-   }
-
-   public void saveLocation(Location location)
-   {
-      db.getLocationDB().saveData(location);
-   }
-
-   public void removeLocation(Location location)
-   {
-      db.getLocationDB().removeData(location);
-   }
-
-   public void saveCourse(Course course)
-   {
-      assert(course.getLength() >= 0);
-      assert(course.getDays() != null);
-      db.getCourseDB().saveData(course);
-   }
-
-   public void removeCourse(Course course)
-   {
-      db.getCourseDB().removeData(course);
-   }
-
-   public int copySchedule(int existingScheduleID, String scheduleName)
-   {
-      return db.copySchedule(existingScheduleID, scheduleName);
-   }
-
-   public String exportToCSV()
-   {
-      try
-      {
-         return new CSVExporter().export(this);
-      }
-      catch (IOException e)
-      {
-         e.printStackTrace();
-         return null;
-      }
-   }
-
-   public void saveSchedule(Schedule s)
-   {
-      db.getScheduleDB().saveData(s);
-   }
-
-   public void saveCurrentScheduleAs(String schedulename)
-   {
-      Schedule current = getSchedule();
-      // Rename current schedule and change dbid so that a new one is added and
-      // not the current one edited instead
-      current.setName(schedulename);
-      current.setScheduleDBId(-1);
-      current.setDbid(-1);
-      db.getScheduleDB().saveData(current);
-      db.setScheduleID(db.getScheduleDB().getScheduleDBID());
-   }
-
-   public void deleteSchedule(String s)
-   {
-      System.out.println("Trying to remove schedule: " + s);
-      UserData userdata = sched_map.get(s);
-      int sid = userdata.getScheduleDBId();
-      Schedule sched = new Schedule();
-      sched.setScheduleDBId(sid);
-      sched.setDbid(sid);
-      sched.setName(s);
-
-      db.getScheduleDB().removeData(sched);
-   }
-
-   public Schedule openNewScheduleFromCSV(String newScheduleName, String value)
-   {
-      try
-      {
-         return new CSVImporter().read(this, newScheduleName, value);
-      }
-      catch (IOException e)
-      {
-         e.printStackTrace();
-         return null;
-      }
-   }
+	public Document findDocumentByID(int documentID) throws NotFoundException {
+		return new Document(database.findDocumentByID(documentID));
+	}
 }
