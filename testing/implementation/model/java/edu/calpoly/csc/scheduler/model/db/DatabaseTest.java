f36728e71fb81d133fa0420120b55092bc48abe5
gfisher <gfisher@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-15 18:55:59 +0000

    Initial checkin.
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1886 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

---
+++ testing/implementation/model/java/edu/calpoly/csc/scheduler/model/db/DatabaseTest.java
@@ -0,0 +1,40 @@
+package edu.calpoly.csc.scheduler.model.db;
+/****
+ *
+ * This is skeleton testing class for Pair.java.  The path to this testing
+ * class parallels exactly the path to the class under test.  I.e., the full
+ * path to Pair.java is
+ *
+ *     scheduler/testing/implementation/GWTView/src/
+ *         edu/calpoly/csc/scheduler/view/web/shared/PairTest.java
+ *
+ * The full path to this file, PairTest.java, is
+ *
+ *     scheduler/testing/testing/implementation/GWTView/src/
+ *         edu/calpoly/csc/scheduler/view/web/shared/PairTest.java
+ *
+ * There is a considerable amount of superfluous directory structure in these
+ * paths.  This can be cleaned when and if there is an overall refactoring of
+ * the project repository.
+ *
+ * @author: Gene Fisher (gfisher@calpoly.edu)
+ * @version: 9feb12
+ */
+public class PairTest extends TestCase {
+
+    /**
+     * Stub for testing the constructor.
+     */
+    public void testDatabase() {}
+
+    /**
+     * Stub for testing the getSchedules access method.
+     */
+    public void testGetLeft() {}
+
+    /**
+     * Stub for testing the getRight access method.
+     */
+    public void testCopyScheduler() {}
+
+}
