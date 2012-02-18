26b4719708627c4d03e7529a1d0e0cae082c8bb3
gfisher <gfisher@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-18 01:13:40 +0000

    Fixed some comments, but this class will likely be nuked in Evan's new design.
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1909 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- testing/implementation/model/java/edu/calpoly/csc/scheduler/model/db/DatabaseTest.java
+++ testing/implementation/model/java/edu/calpoly/csc/scheduler/model/db/DatabaseTest.java
@@ -1,17 +1,17 @@
 package edu.calpoly.csc.scheduler.model.db;
 /****
  *
- * This is skeleton testing class for Pair.java.  The path to this testing
+ * This is skeleton testing class for Database.java.  The path to this testing
  * class parallels exactly the path to the class under test.  I.e., the full
- * path to Pair.java is
+ * path to Database.java is
  *
  *     scheduler/testing/implementation/GWTView/src/
- *         edu/calpoly/csc/scheduler/view/web/shared/PairTest.java
+ *         edu/calpoly/csc/scheduler/view/web/shared/DatabaseTest.java
  *
- * The full path to this file, PairTest.java, is
+ * The full path to this file, DatabaseTest.java, is
  *
  *     scheduler/testing/testing/implementation/GWTView/src/
- *         edu/calpoly/csc/scheduler/view/web/shared/PairTest.java
+ *         edu/calpoly/csc/scheduler/view/web/shared/DatabaseTest.java
  *
  * There is a considerable amount of superfluous directory structure in these
  * paths.  This can be cleaned when and if there is an overall refactoring of
@@ -20,7 +20,7 @@ package edu.calpoly.csc.scheduler.model.db;
  * @author: Gene Fisher (gfisher@calpoly.edu)
  * @version: 9feb12
  */
-public class PairTest extends TestCase {
+public class DatabaseTest extends TestCase {
 
     /**
      * Stub for testing the constructor.
