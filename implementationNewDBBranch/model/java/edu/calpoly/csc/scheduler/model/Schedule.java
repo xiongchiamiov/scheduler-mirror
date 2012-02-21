0fdd6bcd16924b20a163a69f7a206e8e51fbac58
tyhollan <tyhollan@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-21 19:21:11 +0000

    Fixed some imports
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1938 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/Schedule.java
+++ implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/Schedule.java
@@ -1,11 +1,6 @@
 package edu.calpoly.csc.scheduler.model;
 
-import java.util.Collection;
-import java.util.Set;
-
 import edu.calpoly.csc.scheduler.model.db.IDBSchedule;
-import edu.calpoly.csc.scheduler.model.db.IDBScheduleItem;
-import edu.calpoly.csc.scheduler.view.web.shared.ScheduleItemGWT;
 
 public class Schedule {
 	IDBSchedule underlyingSchedule;
--- implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/ScheduleItem.java
+++ implementationNewDBBranch/model/java/edu/calpoly/csc/scheduler/model/ScheduleItem.java
@@ -1,11 +1,8 @@
 package edu.calpoly.csc.scheduler.model;
 
-import java.util.Collection;
 import java.util.Set;
 
-import edu.calpoly.csc.scheduler.model.db.IDBSchedule;
 import edu.calpoly.csc.scheduler.model.db.IDBScheduleItem;
-import edu.calpoly.csc.scheduler.view.web.shared.ScheduleItemGWT;
 
 public class ScheduleItem {
 	IDBScheduleItem underlying;
