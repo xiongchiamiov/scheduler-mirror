aefd96b54836c215a712000ec89e3e0f37db6d12
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-02-22 20:15:17 +0000

    fixed array clone
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@1970 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementationNewDBBranch/GWTView/src/edu/calpoly/csc/scheduler/view/web/shared/InstructorGWT.java
+++ implementationNewDBBranch/GWTView/src/edu/calpoly/csc/scheduler/view/web/shared/InstructorGWT.java
@@ -6,6 +6,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+import edu.calpoly.csc.scheduler.model.Day;
+
 
 public class InstructorGWT implements Serializable, Identified {
 	private static final long serialVersionUID = -4982539363690274674L;
@@ -41,7 +43,13 @@ public class InstructorGWT implements Serializable, Identified {
 
 	public InstructorGWT(InstructorGWT that) {
 		this(that.id, that.username, that.firstName, that.lastName,
-				that.maxwtu, that.tPrefs.clone(), null);
+				that.maxwtu, null, null);
+		
+		int newTimePrefs[][] = new int[Day.values().length][48];
+		for (Day day : Day.values())
+			for (int halfHour = 0; halfHour < 48; halfHour++)
+				newTimePrefs[day.ordinal()][halfHour] = that.tPrefs[day.ordinal()][halfHour];
+		tPrefs = newTimePrefs;
 		
 		HashMap<Integer, Integer> newCoursePrefs = new LinkedHashMap<Integer, Integer>(that.coursePrefs);
 		for (Integer course : that.coursePrefs.keySet())
