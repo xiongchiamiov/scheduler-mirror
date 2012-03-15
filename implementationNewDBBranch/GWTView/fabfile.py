090fcf3f1593def6268f6e0956a550131e666002
pearson <pearson@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-03-15 23:28:00 +0000

    automatically create .properties files on deploy
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2126 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementationNewDBBranch/GWTView/fabfile.py
+++ implementationNewDBBranch/GWTView/fabfile.py
@@ -20,6 +20,14 @@ def deploy(*directories):
 	#build()
 	for directory in directories:
 		print('Deploying %s...' % directory)
+		local('''echo "#this required property tells the scheduler where to write its database file to.
+		databasefilepath=/var/lib/tomcat6/webapps/DEPT/DatabaseState.javaser
+
+		#this optional property tells the scheduler to feed databasefilepath to
+		# the getServletContext().getRealPath(...) function. default is true.
+		#applyServletPath=true
+		applyServletPath=false" > war/WEB-INF/classes/edu/calpoly/csc/scheduler/view/web/server/scheduler.properties''')
+		local("sed -i 's/DEPT/%s/' war/WEB-INF/classes/edu/calpoly/csc/scheduler/view/web/server/scheduler.properties" % directory)
 		put('war/*', '/var/lib/tomcat6/webapps/'+directory+'/')
 	restart_tomcat()
 
