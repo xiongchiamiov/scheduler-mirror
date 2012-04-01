1f0fa650c038b21f127c48a0f44b1a2ca9750ff9
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-01 01:48:57 +0000

    fixed instructor prefs zindex thing
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2199 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementationBranchNewTable/src/scheduler/view/web/client/views/resources/instructors/InstructorsView.java
+++ implementationBranchNewTable/src/scheduler/view/web/client/views/resources/instructors/InstructorsView.java
@@ -8,7 +8,6 @@ import scheduler.view.web.client.ViewFrame;
 import scheduler.view.web.shared.DocumentGWT;
 import scheduler.view.web.shared.InstructorGWT;
 
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
@@ -20,12 +19,13 @@ import com.google.gwt.user.client.ui.Widget;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.ListGridEditEvent;
 import com.smartgwt.client.types.RowEndEditAction;
-import com.smartgwt.client.util.SC;
+import com.smartgwt.client.types.VerticalAlignment;
 import com.smartgwt.client.widgets.Canvas;
-import com.smartgwt.client.widgets.IButton;  
-import com.smartgwt.client.widgets.ImgButton;  
-import com.smartgwt.client.widgets.events.ClickEvent;  
-import com.smartgwt.client.widgets.events.ClickHandler; 
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.Window;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
@@ -34,13 +34,13 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 	private GreetingServiceAsync service;
 	private final DocumentGWT document;
 	private ViewFrame frame;
-
+	
 	public InstructorsView(GreetingServiceAsync service, DocumentGWT document) {
 		this.service = service;
 		this.document = document;
 		// this.addStyleName("iViewPadding");
 	}
-
+	
 	@Override
 	public boolean canPop() {
 		return true;
@@ -50,55 +50,48 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 		// return
 		// Window.confirm("You have unsaved data which will be lost. Are you sure you want to navigate away?");
 	}
-
+	
 	@Override
 	public void afterPush(ViewFrame frame) {
 		this.frame = frame;
-
+		
 		this.setWidth("100%");
 		this.setHeight("100%");
-
+		
 		this.add(new HTML("<h2>Instructors</h2>"));
-
+		
 		final ListGrid grid = new ListGrid() {
 			@Override
-			protected Canvas createRecordComponent(final ListGridRecord record,
-					Integer colNum) {
+			protected Canvas createRecordComponent(final ListGridRecord record, Integer colNum) {
 				String fieldName = this.getFieldName(colNum);
 				if (fieldName.equals("instructorPrefs")) {
-					IButton button = new IButton();  
-                    button.setHeight(18);  
-                    button.setWidth(65);                      
-                    button.setTitle("Preferences");  
-                    button.addClickHandler(new ClickHandler() {  
-                        public void onClick(ClickEvent event) {  
-							final int instructorID = record
-									.getAttributeAsInt("id");
-							service.getInstructorsForDocument(
-									document.getID(),
-									new AsyncCallback<List<InstructorGWT>>() {
-										public void onFailure(
-												Throwable caught) {
-											Window.alert("failed to get instructors!");
+					IButton button = new IButton();
+					button.setHeight(18);
+					button.setWidth(65);
+					button.setTitle("Preferences");
+					button.addClickHandler(new ClickHandler() {
+						public void onClick(ClickEvent event) {
+							final int instructorID = record.getAttributeAsInt("id");
+							service.getInstructorsForDocument(document.getID(), new AsyncCallback<List<InstructorGWT>>() {
+								public void onFailure(Throwable caught) {
+									com.google.gwt.user.client.Window.alert("failed to get instructors!");
+								}
+								
+								@Override
+								public void onSuccess(List<InstructorGWT> result) {
+									for (InstructorGWT instructor : result) {
+										if (instructor.getID().equals(instructorID)) {
+											preferencesButtonClicked(instructor);
+											break;
 										}
-
-										@Override
-										public void onSuccess(
-												List<InstructorGWT> result) {
-											for (InstructorGWT instructor : result) {
-												if (instructor
-														.getID()
-														.equals(instructorID)) {
-													preferencesButtonClicked(instructor);
-													break;
-												}
-											}
-										}
-									});
-                        }  
-                    });  
-                    return button;
-				} else {
+									}
+								}
+							});
+						}
+					});
+					return button;
+				}
+				else {
 					return null;
 				}
 			}
@@ -115,10 +108,10 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 		grid.setDataSource(new InstructorsDataSource(service, document));
 		grid.setShowRecordComponents(true);
 		grid.setShowRecordComponentsByCell(true);
-
+		
 		ListGridField idField = new ListGridField("id");
 		idField.setHidden(true);
-
+		
 		ListGridField usernameField = new ListGridField("username", "Username");
 		ListGridField firstNameField = new ListGridField("firstName",
 				"First Name");
@@ -127,19 +120,19 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 		ListGridField instructorPrefsField = new ListGridField(
 				"instructorPrefs", "Preferences");
 		instructorPrefsField.setAlign(Alignment.CENTER);
-
+		
 		grid.setFields(idField, usernameField, firstNameField, lastNameField,
 				maxWTUField, instructorPrefsField);
-
+		
 		this.add(grid);
-
+		
 		this.add(new Button("Add New Instructor", new com.google.gwt.event.dom.client.ClickHandler() {
 			@Override
 			public void onClick(com.google.gwt.event.dom.client.ClickEvent event) {
 				grid.startEditingNew();
 			}
 		}));
-
+		
 		this.add(new Button("Remove Selected Instructors", new com.google.gwt.event.dom.client.ClickHandler() {
 			@Override
 			public void onClick(com.google.gwt.event.dom.client.ClickEvent event) {
@@ -150,24 +143,45 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 			}
 		}));
 	}
-
+	
 	@Override
-	public void beforePop() {
-	}
-
+	public void beforePop() {}
+	
 	@Override
-	public void beforeViewPushedAboveMe() {
-	}
-
+	public void beforeViewPushedAboveMe() {}
+	
 	@Override
-	public void afterViewPoppedFromAboveMe() {
-	}
-
+	public void afterViewPoppedFromAboveMe() {}
+	
 	@Override
 	public Widget getContents() {
 		return this;
 	}
+	
+	public static Window createWin(String title, boolean autoSizing, int width, int height, int offsetLeft) {  
+      Label label = new Label(  
+              "<b>Severity 1</b> - Critical problem<br/>System is unavailable in production or is corrupting data, and the error severely impacts the user's operations.<br/><br/>"  
+                      + "<b>Severity 2</b> - Major problem<br/>An important function of the system is not available in production, and the user's operations are restricted.<br/><br/>"  
+                      + "<b>Severity 3</b> - Minor problem<br/>Inability to use a function of the system occurs, but it does not seriously affect the user's operations.");  
+      label.setWidth100();  
+      label.setHeight100();  
+      label.setPadding(5);  
+      label.setValign(VerticalAlignment.TOP);  
 
+      Window window = new Window();  
+      window.setAutoSize(autoSizing);  
+      window.setTitle(title);  
+      window.setWidth(width);  
+      window.setHeight(height);  
+      window.setLeft(offsetLeft);  
+      window.setCanDragReposition(true);  
+      window.setCanDragResize(true);  
+      window.addItem(label);  
+
+      return window;  
+  }  
+	
+	
 	public void preferencesButtonClicked(InstructorGWT instructor) {
 		if (frame.canPopViewsAboveMe()) {
 			// viewFrame.popFramesAboveMe();
@@ -185,29 +199,37 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 			// viewFrame.add(ipv);
 			// ipv.setHeight("500px");
 			// ipv.setWidth("500px");
-			final DialogBox poopyhead = new DialogBox();
+			
+			final Window window = new Window();
+			window.setAutoSize(true);
+			window.setTitle("Instructor Preferences");
+			window.setCanDragReposition(true);
+			window.setCanDragResize(true);
+			
+			
+			
 			iipv.afterPush();
 			final ScrollPanel weewee = new ScrollPanel();
 			weewee.setWidget(iipv);
 			weewee.setSize("700px", "600px");
-			poopyhead.add(weewee);
+			window.addItem(weewee);
 			// poopyhead.setTitle(instructor.getFirstName() + " " +
 			// instructor.getLastName() + "'s Preferences");
 			// iipv.setVisible(true);
 			// ipv.add(new Button("OK"));
 			// ipv.add(iipv);
 			// poopyhead.add(iipv);
-			poopyhead.setPopupPosition(600, 0);
+//			poopyhead.setPopupPosition(600, 0);
 			// poopyhead.add
 			ClickListener listener = new ClickListener() {
 				public void onClick(Widget sender) {
-					poopyhead.hide();
+					window.hide();
 				}
 			};
 			Button button = new Button("Close", listener);
 			iipv.add(button);
 			button.setStyleName("centerness");
-			poopyhead.show();
+			window.show();
 			// ipv.setWidget(iipv);
 			// ipv.setVisible(true);
 			// this.add(ipv);
@@ -222,5 +244,5 @@ public class InstructorsView extends VerticalPanel implements IViewContents {
 			System.out.println("ABANDON SHIP CAPTAIN");
 		}
 	}
-
+	
 }
