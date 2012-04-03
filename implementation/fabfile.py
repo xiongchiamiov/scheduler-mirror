7d219a1a1eb18248e01e8ee87283b38b818b36de
pearson <pearson@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-03 01:41:33 +0000

    fix command-line building after project reorganization
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2353 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/fabfile.py
+++ implementation/fabfile.py
@@ -21,13 +21,12 @@ def deploy(*directories):
 	for directory in directories:
 		print('Deploying %s...' % directory)
 		local('''echo "#this required property tells the scheduler where to write its database file to.
-		databasefilepath=/var/lib/tomcat6/webapps/DEPT/DatabaseState.javaser
+		databasefilepath=/var/lib/tomcat6/webapps/%s/DatabaseState.javaser
 
 		#this optional property tells the scheduler to feed databasefilepath to
 		# the getServletContext().getRealPath(...) function. default is true.
 		#applyServletPath=true
-		applyServletPath=false" > war/WEB-INF/classes/edu/calpoly/csc/scheduler/view/web/server/scheduler.properties''')
-		local("sed -i 's/DEPT/%s/' war/WEB-INF/classes/edu/calpoly/csc/scheduler/view/web/server/scheduler.properties" % directory)
+		applyServletPath=false" > war/WEB-INF/classes/scheduler/view/web/server/scheduler.properties''' % directory)
 		put('war/*', '/var/lib/tomcat6/webapps/'+directory+'/')
 	restart_tomcat()
 
