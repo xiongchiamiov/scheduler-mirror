196161058cae09560bd38071262c766de88ffc9a
krscanlo <krscanlo@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-09 19:45:48 +0000

    .sql statments
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2444 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/src/scheduler/view/web/client/UpdateHeaderStrategy.java
+++ implementation/src/scheduler/view/web/client/UpdateHeaderStrategy.java
@@ -3,7 +3,7 @@ package scheduler.view.web.client;
 public interface UpdateHeaderStrategy {
 	public void clearHeader();
 	public void onLogin(String username);
-	public void onOpenedDocument(String documentName);
+	public void onOpenedDocument(String documentName);    
 	public void onDocumentNameChanged(String newDocumentName);
 	public void setDocumentChanged(boolean documentChanged); 
 }
