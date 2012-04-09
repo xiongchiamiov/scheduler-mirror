01c4dba79a62099db9dd24af856f4aff3a773d75
snavarre <snavarre@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-09 18:22:30 +0000

    added element IDs to list grids
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2434 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- implementation/src/scheduler/view/web/client/ExportDialog.java
+++ implementation/src/scheduler/view/web/client/ExportDialog.java
@@ -4,6 +4,7 @@ import scheduler.view.web.client.views.resources.instructors.InstructorPreferenc
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FocusPanel;
@@ -26,14 +27,20 @@ public class ExportDialog {
 		window.setTitle("Export As");
 		window.setCanDragReposition(true);
 		window.setCanDragResize(true);
-		
+		window.setID("s_exportPopup");
 		
 		VerticalPanel mainVerticalPanel = new VerticalPanel();
+		DOM.setElementAttribute(mainVerticalPanel.getElement(), "id", "s_mainExportPanel");
 		
 		VerticalPanel verticalPanel = new VerticalPanel();
+	    DOM.setElementAttribute(verticalPanel.getElement(), "id", "s_exportPanel");
+	    
 		mainVerticalPanel.add(verticalPanel);
 		
 		HorizontalPanel typeSelectorPanel = new HorizontalPanel();
+	    
+		DOM.setElementAttribute(typeSelectorPanel.getElement(), "id", "s_typeSelectorPanel");
+		
 		verticalPanel.add(typeSelectorPanel);
 		
 		final FocusPanel csvFocusPanel = new FocusPanel();
@@ -43,12 +50,16 @@ public class ExportDialog {
 			}
 		});
 		
+		DOM.setElementAttribute(csvFocusPanel.getElement(), "id", "s_csvFocusPanel");
+		
 		FocusPanel pdfFocusPanel = new FocusPanel();
 		pdfFocusPanel.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				//TODO Save Selection
 			}
 		});
+		
+		DOM.setElementAttribute(pdfFocusPanel.getElement(), "id", "s_pdfFocusPanel");
 		
 		pdfFocusPanel.setStyleName("exportChoice");
 		typeSelectorPanel.add(pdfFocusPanel);
@@ -58,11 +69,17 @@ public class ExportDialog {
 		pdfFocusPanel.setWidget(pdfPanel);
 		pdfPanel.setSize("100px", "100px");
 		
+		DOM.setElementAttribute(pdfPanel.getElement(), "id", "s_pdfPanel");
+		
 		Image PDFIcon = new Image("imgs/pdf-icon.png");
 		pdfPanel.add(PDFIcon);
 		PDFIcon.setSize("100px", "100px");
 		
+		DOM.setElementAttribute(PDFIcon.getElement(), "id", "s_pdfIcon");
+		
 		Label lblCSV = new Label("PDF");
+		DOM.setElementAttribute(lblCSV.getElement(), "id", "s_csvLbl");
+		
 		pdfPanel.add(lblCSV);
 		pdfPanel.setCellHorizontalAlignment(lblCSV, HasHorizontalAlignment.ALIGN_CENTER);
 		csvFocusPanel.setStyleName("exportChoice");
@@ -75,10 +92,13 @@ public class ExportDialog {
 		csvPanel.setSize("100px", "100px");
 
 		Image CSVIcon = new Image("imgs/csv-icon.png");
+		DOM.setElementAttribute(CSVIcon.getElement(), "id", "s_csvIcon");
+		
 		csvPanel.add(CSVIcon);
 		CSVIcon.setSize("100px", "100px");
 
 		Label lblNewLabel = new Label("Excel (CSV)");
+		DOM.setElementAttribute(lblNewLabel.getElement(), "id", "s_excelCSVLbl");
 		csvPanel.add(lblNewLabel);
 		csvPanel.setCellHorizontalAlignment(lblNewLabel, HasHorizontalAlignment.ALIGN_CENTER);
 		
@@ -98,6 +118,7 @@ public class ExportDialog {
 			}
 		});
 		
+		DOM.setElementAttribute(cancelButton.getElement(), "id", "s_cancelExportBtn");
 		
 		final Button nextButton = new Button("Next", new ClickHandler() {
 			@Override
@@ -119,6 +140,8 @@ public class ExportDialog {
 				TODOmainVerticalPanel.add(lblTODO);
 				
 				TODOdb.setText("Not yet implemented");
+				
+				//DOM.setElementAttribute(verticalPanel.getElement(), "id", "s_exportPanel");
 				
 				final Button TODOcancelButton = new Button("Cancel", new ClickHandler() {
 					@Override
