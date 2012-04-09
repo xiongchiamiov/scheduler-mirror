99d049fda0567c61f7c71566a559426c1bcf5cd9
snavarre <snavarre@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-09 18:44:13 +0000

    sample test stuff
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2435 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/src/scheduler/view/web/shared/Selenium/SchedulerBotTest.java
+++ Selenium/src/scheduler/view/web/shared/Selenium/SchedulerBotTest.java
@@ -1,5 +1,9 @@
 package scheduler.view.web.shared.Selenium;
 
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -21,22 +25,50 @@ public class SchedulerBotTest extends TestCase {
 	}
 	
 	public void testFirefoxDriver() {
-		//this.fbot = new FirefoxDriver();
+		this.fbot = new FirefoxDriver();
 		
 		//this.iebot = new InternetExplorerDriver();
 		//driver.get("http://www.google.com");
 		//driver.close();
 		
-		//fbot.get("http://127.0.0.1:8888/Scheduler.html?gwt.codesvr=127.0.0.1:9997");
-		//fbot.close();
+		WebElement loginBtn, unameField, appTitle, logTitle;
+		//some url not localhost
+		fbot.get("http://www.scheduler.csc.calpoly.edu/dev");
+
+			loginBtn = fbot.findElement(By.id("s_loginBtn")); 
+			appTitle = fbot.findElement(By.id("appNameTtl"));
+			assertEquals("Schedulizerifier", appTitle.getText());
+			assertEquals("Login", loginBtn.getText());
+			unameField = fbot.findElement(By.id("s_unameBox"));
+		    logTitle = fbot.findElement(By.id("s_LoginTag"));
+		    assertEquals("Login", logTitle.getText());
+		    System.out.println("logTitle text: " + logTitle.getText());
+			
+			unameField.sendKeys("snavarre");
+			loginBtn.click();
+
+		    
+			
+			WebElement title = fbot.findElement(By.id("s_loginTag"));
+			assertEquals("Login", title.getText());
+
+			WebElement user = fbot.findElement(By.id("s_unameLbl"));
+			assertEquals("snavarre", user.getText());
+			WebElement logout = fbot.findElement(By.id("s_logoutLnk"));
+			WebElement hometab = fbot.findElement(By.id("s_HomeTab"));
+			assertEquals("Home", hometab.getText());
+			
+			logout.click();
+
+		fbot.close();
 	}
 	
 	public void testChromeDriver() {
 		String path = this.getClass().getClassLoader().getResource("chromedriver").getPath(); 
-		System.setProperty("webdriver.chrome.driver", path); 
-		System.out.println(path);
-		this.cbot = new ChromeDriver();
-		cbot.get("http://127.0.0.1:8888/Scheduler.html?gwt.codesvr=127.0.0.1:9997");
-		cbot.close();
+//		System.setProperty("webdriver.chrome.driver", path); 
+//		System.out.println(path);
+//		this.cbot = new ChromeDriver();
+//		cbot.get("http://www.google.com");
+//		cbot.close();
 	}
 }
