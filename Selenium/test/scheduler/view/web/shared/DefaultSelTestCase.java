915ce6088de2f6fc323a2b8327ac59139f361e15
snavarre <snavarre@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-11 18:32:26 +0000

    follow FirefoxLoginTest for sel test formatting
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2458 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/test/scheduler/view/web/shared/DefaultSelTestCase.java
+++ Selenium/test/scheduler/view/web/shared/DefaultSelTestCase.java
@@ -1,5 +1,7 @@
 package scheduler.view.web.shared;
 
+import org.openqa.selenium.WebDriver;
+
 import junit.framework.TestCase;
 //import org.junit.*;
 //import org.junit.runner.RunWith;
@@ -15,25 +17,22 @@ import scheduler.view.web.shared.Selenium.*;
 public class DefaultSelTestCase extends TestCase {
 	
 	/** The bot in charge of navigating the Scheduler GUI. */
-	SchedulerBot bot;
-
+	//SchedulerBot bot;
+	private WebDriver driver;
+	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
-	//@Before
-	public void setUp(String url) {
-		if(url.equals(""))
-			bot = new SchedulerBot();
-		else
-			bot = new SchedulerBot(url);
+	public void setUp(String url, WebDriver drv) {
+		drv.get(url);
 	}
 	
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#tearDown()
 	 */
-	//@After
 	public void tearDown() {
 		/** cleanup artifacts and quit the browser session for the current test*/
-		bot.quitSession();
+		driver.close();
+		driver.quit();
 	}
 }
