76fdeb8d57fc905348c3a6f2c2be16f22e7b127e
snavarre <snavarre@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-11 17:30:27 +0000

    testsuites for model/model db
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2454 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

---
+++ implementation/test/scheduler/model/ModelTestSuite.java
@@ -0,0 +1,37 @@
+package scheduler.model;
+
+import junit.framework.TestSuite;
+
+public class ModelTestSuite {
+
+	/**
+	 * The testsuite meant to fully exercise model code 
+	 * Current code coverage:
+	 *
+	 * @param name the name
+	 */
+	public ModelTestSuite(String name) {
+		super();
+	}
+	
+	/**
+	 * Suite.
+	 *
+	 * @return the test suite
+	 */
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite("Model tests");
+		suite.addTest(new AlgorithmTestSimpleDB());
+		suite.addTest(new CoursesTestSimpleDB());
+		suite.addTest(new DocumentsTestSimpleDB());
+		suite.addTest(new DocumentsTestSQLDB());
+		suite.addTest(new InstructorsPreferencesTestSimpleDB());
+		suite.addTest(new InstructorsTestSimpleDB());
+		suite.addTest(new InstructorsTestSQLDB());
+		suite.addTest(new LocationsTestSimpleDB());
+		suite.addTest(new ScheduleItemsTestSimpleDB());
+		suite.addTest(new UsersTestSimpleDB());
+		
+		return suite;
+	}
+}
