a3d9fad09214fc4a0b760b5f46daca455bf5965b
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-13 21:50:48 +0000

    fixed test, now uses xpath
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2484 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/test/scheduler/view/web/shared/FirefoxLoginTest.java
+++ Selenium/test/scheduler/view/web/shared/FirefoxLoginTest.java
@@ -1,6 +1,7 @@
 package scheduler.view.web.shared;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -50,12 +51,48 @@ public class FirefoxLoginTest extends DefaultSelTestCase {
 		WebElement user = driver.findElement(By.id("s_unameLbl"));
 		assertEquals("snavarre", user.getText());
 		WebElement logout = driver.findElement(By.id("s_logoutLnk"));
-		WebElement hometab = driver.findElement(By.id("s_HomeTab"));
+		
+		
+		
+		// Unfortunately, my CSS idea didnt work, it's retardedly hard to set CSS styles (you can do it,
+		// but it requires calling getElement(), which lazily generates an element, which has side effects
+		// regarding layout). So we need to use XPath.
+
+		// The secret ingredient for finding an element we set the ID on is the eventproxy attribute.
+		// When we call setID on something in smartgwt, it sets the eventproxy attribute to that string.
+		// (in case you're curious, eventproxy refers to a javascript global. you can see this by going
+		// into the browser and typing "javascript:void(window.alert(s_HomeTab))" without the quotes.
+		// I think we can rely on eventproxy always being there, because setID is how user *javascript*
+		// is supposed to access the object.)
+		
+		WebElement hometab = driver.findElement(By.xpath("//div[@eventproxy='s_HomeTab']"));
+		// Home tab is a gigantic element containing another div, which contains a table, which eventually contains "Home".
+		// Calling getText() on such a complex element just returns all the getText()'s of the children appended together, that's why this works.
 		assertEquals("Home", hometab.getText());
+
+		// If we want to be super specific, we can do this.
+		WebElement labelInsideHomeTab = driver.findElement(By.xpath("//div[@eventproxy='s_HomeTab']/div/table/tbody/tr/td"));
+		assertEquals("Home", labelInsideHomeTab.getText());
+		
+		
+		// I just made a helper function called getElementBySmartGWTID below, you can use it like this
+		// to achieve the same effect:
+		WebElement homeTabGottenFromMyFunc = getElementBySmartGWTID("s_HomeTab");
+		assertEquals("Home", homeTabGottenFromMyFunc.getText());
+		
+		// Again, using my function, but being more specific about which element's contents we want to check:
+		WebElement labelInsideHomeTabGottenFromMyFunc = getElementBySmartGWTID("s_HomeTab").findElement(By.xpath("div/table/tbody/tr/td"));
+		assertEquals("Home", labelInsideHomeTabGottenFromMyFunc.getText());
+		
+		
 		
 		logout.click();
 
 		
+	}
+	
+	private WebElement getElementBySmartGWTID(String smartGWTID) {
+		return driver.findElement(By.xpath("//div[@eventproxy='" + smartGWTID + "']"));
 	}
 	
 	/**
