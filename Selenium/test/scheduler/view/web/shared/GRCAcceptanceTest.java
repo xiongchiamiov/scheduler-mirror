7cdd98b74d2c8748d679a2249043f47d11c72278
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-15 21:23:01 +0000

    did some work on my acceptance test
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2486 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/test/scheduler/view/web/shared/GRCAcceptanceTest.java
+++ Selenium/test/scheduler/view/web/shared/GRCAcceptanceTest.java
@@ -1,9 +1,16 @@
 package scheduler.view.web.shared;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Predicate;
 
 public abstract class GRCAcceptanceTest extends DefaultSelTestCase {	
 	private WebDriver driver;
@@ -37,55 +44,123 @@ public abstract class GRCAcceptanceTest extends DefaultSelTestCase {
 		}
 	}
 	
+	interface Waitable {
+		public boolean stopWaiting();
+	}
+	
+	private void waitForElementPresent(final By by) throws InterruptedException {
+		new WebDriverWait(driver, 60).until(new Predicate<WebDriver>() {
+			public boolean apply(WebDriver arg0) {
+				try {
+					if (isElementPresent(by))
+						return true;
+				} catch (Exception e) {}
+				return false;
+			}
+		});
+	}
+	
+	private void mouseDownAndUpAt(By by, int x, int y) {
+		WebElement element = driver.findElement(by);
+
+		new Actions(driver)
+				.clickAndHold(element)
+				.moveByOffset(x, y)
+				.release(element)
+				.build()
+				.perform(); 
+	}
+	
+	class PopupWaiter {
+		final Set<String> initialWindows;
+		String poppedUpWindow;
+		public PopupWaiter() {
+			initialWindows = driver.getWindowHandles();
+		}
+		String waitForPopup() {
+			new WebDriverWait(driver, 20).until(new Predicate<WebDriver>() {
+				public boolean apply(WebDriver arg0) {
+					Set<String> currentWindows = driver.getWindowHandles();
+					currentWindows.removeAll(initialWindows);
+					if (!currentWindows.isEmpty()) {
+						poppedUpWindow = currentWindows.iterator().next();
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			});
+			assert(poppedUpWindow != null);
+			return poppedUpWindow;
+		}
+	}
+	
+	private WebElement elementForResourceTableCell(int row0Based, int col0Based) {
+		return driver.findElement(By.xpath("((//table[@class='listTable']/tbody/tr[@role='listitem'])[" + (1 + row0Based) + "]/td)[" + (1 + col0Based) + "]"));
+	}
+	
+	private void enterIntoResourceTableCell(int row0Based, int col0Based, String text) {
+		WebElement newCourseDeptCell = elementForResourceTableCell(row0Based, col0Based);
+		newCourseDeptCell.click();
+		
+		WebElement input = elementForResourceTableCell(0, 2).findElement(By.xpath("//input"));
+		input.sendKeys(text);
+
+		driver.findElement(By.tagName("body")).click();
+	}
+	
+	private void enterIntoResourceTableRow(int row0Based, Object... values) {
+		for (int i = 0; i < values.length; i++) {
+			Object object = values[i];
+			if (object == null)
+				;
+			else if (object instanceof String)
+				enterIntoResourceTableCell(row0Based, i, (String)object);
+			else
+				assert(false);
+		}
+	}
+	
+	private void enterIntoResourceTableNewRow(int row0Based, Object...values) {
+		driver.findElement(By.id("s_newCourseBtn")).click();
+		enterIntoResourceTableRow(row0Based, values);
+	}
+	
 	/**
 	 * Test initializing page elements
 	 * @throws InterruptedException 
 	 */
 	public void testAcceptanceForGRC() throws InterruptedException {
+		final String documentName = "Winter 2012x";
+		
 		driver.get(protoURL);
 		assert(driver.findElement(By.id("s_unameBox")) != null);
 		driver.findElement(By.id("s_unameBox")).clear();
 		driver.findElement(By.id("s_unameBox")).sendKeys("eovadia");
 		driver.findElement(By.id("s_loginBtn")).click();
-		// ERROR: Caught exception [ERROR: Unsupported command [isTextPresent]]
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (isElementPresent(By.xpath("//div[@eventproxy='s_createBtn']"))) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
+		waitForElementPresent(By.xpath("//div[@eventproxy='s_createBtn']"));
+		
+		mouseDownAndUpAt(By.xpath("//div[@eventproxy='s_createBtn']"), 5, 5);
 
-//		// ERROR: Caught exception [ERROR: Unsupported command [mouseDownAt]]
-//		// ERROR: Caught exception [ERROR: Unsupported command [mouseUpAt]]
-//		for (int second = 0;; second++) {
-//			if (second >= 60) fail("timeout");
-//			try { if (isElementPresent(By.id("s_createBox"))) break; } catch (Exception e) {}
-//			Thread.sleep(1000);
-//		}
-//
-//		driver.findElement(By.id("s_createBox")).clear();
-//		driver.findElement(By.id("s_createBox")).sendKeys("Winter 2012c");
-//		driver.findElement(By.id("s_createNamedDocBtn")).click();
-//		// ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp]]
-//		// ERROR: Caught exception [ERROR: Unsupported command [selectWindow]]
-//		driver.findElement(By.id("s_newCourseBtn")).click();
-//		driver.findElement(By.id("isc_2M")).clear();
-//		driver.findElement(By.id("isc_2M")).sendKeys("GRC");
-//		driver.findElement(By.id("isc_2P")).clear();
-//		driver.findElement(By.id("isc_2P")).sendKeys("201");
-//		driver.findElement(By.id("isc_2S")).clear();
-//		driver.findElement(By.id("isc_2S")).sendKeys("Graphics");
-//		driver.findElement(By.id("isc_2V")).clear();
-//		driver.findElement(By.id("isc_2V")).sendKeys("3");
-//		driver.findElement(By.id("isc_2Y")).clear();
-//		driver.findElement(By.id("isc_2Y")).sendKeys("4");
-//		driver.findElement(By.id("isc_31")).clear();
-//		driver.findElement(By.id("isc_31")).sendKeys("4");
-//		driver.findElement(By.id("isc_38")).click();
-//		driver.findElement(By.id("isc_3H")).clear();
-//		driver.findElement(By.id("isc_3H")).sendKeys("4");
-//		driver.findElement(By.id("isc_3K")).clear();
-//		driver.findElement(By.id("isc_3K")).sendKeys("20");
-//		driver.findElement(By.xpath("//table[@id='isc_3Otable']/tbody[2]/tr[4]/td[2]")).click();
-//		// ERROR: Caught exception [ERROR: Unsupported command [getAlert]]
+		waitForElementPresent(By.id("s_createBox"));
+		
+		driver.findElement(By.id("s_createBox")).clear();
+		driver.findElement(By.id("s_createBox")).sendKeys(documentName);
+
+		PopupWaiter popupWaiter = new PopupWaiter();
+		
+		driver.findElement(By.id("s_createNamedDocBtn")).click();
+		
+		String newWindowHandle = popupWaiter.waitForPopup();
+		
+		driver.switchTo().window(newWindowHandle);
+
+		// TODO: see if documentName appears anywhere on screen?
+
+		enterIntoResourceTableNewRow(0, null, null, "GRC", "201", "Graphics", "4", "3", "3", null, "4", "30");
+		enterIntoResourceTableNewRow(1, null, null, "GRC", "202", "GraphicsB", "4", "3", "3", null, "4", "30");
+
+		// TODO: save
 	}
 }
