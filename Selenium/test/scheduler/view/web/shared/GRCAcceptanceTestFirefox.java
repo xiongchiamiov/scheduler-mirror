fae6d7386a25b626668ae50f27386e48db588d7a
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-16 20:07:20 +0000

    modified my test
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2502 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/test/scheduler/view/web/shared/GRCAcceptanceTestFirefox.java
+++ Selenium/test/scheduler/view/web/shared/GRCAcceptanceTestFirefox.java
@@ -1,11 +1,6 @@
 package scheduler.view.web.shared;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.pagefactory.ByChained; 
 
 public class GRCAcceptanceTestFirefox extends GRCAcceptanceTest {	
 	public void setUp() {
