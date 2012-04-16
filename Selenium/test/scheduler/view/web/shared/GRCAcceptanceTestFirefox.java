d1a702ee04847116ae096b61a1f9b9e48e2dfff8
eovadia <eovadia@13b268f7-8392-4c9d-bc7d-1e80a86bc2f9>
2012-04-16 20:07:58 +0000

    removing bin
    git-svn-id: svn+ssh://scheduler.csc.calpoly.edu/repo@2504 13b268f7-8392-4c9d-bc7d-1e80a86bc2f9

--- Selenium/test/scheduler/view/web/shared/GRCAcceptanceTestFirefox.java
+++
@@ -1,9 +0,0 @@
-package scheduler.view.web.shared;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class GRCAcceptanceTestFirefox extends GRCAcceptanceTest {	
-	public void setUp() {
-		super.setUp(new FirefoxDriver());
-	}
-}
